1047b5cefeb00e290e0859e609e8b46cf87203f1
Linsa Lison <228749608+linsajo@users.noreply.github.com>
2026-07-28 23:28:26 +0200

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  // This must match your GitHub repository name.
   base: '/portfolio/',
-});
+})
